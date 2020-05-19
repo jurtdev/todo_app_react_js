8d93eb69e693648c1d856774b30bc6a9f7b0788b
Christian Jurt <51300915+jurtdev@users.noreply.github.com>
2020-05-18 22:15:28 -0400

    fixed css bug in footer

--- todo_react_js/src/App.js
+++ todo_react_js/src/App.js
@@ -5,87 +5,87 @@ import './App.css';
 import './Toggle.css';
 
 function App() {
+  function TodoForm({ addTodo }) {
+    const [value, setValue] = useState('');
 
-  function TodoForm ( {addTodo} ) {
-    const [value, setValue] =  useState('');
-
-    const handleSubmit = e => {
+    const handleSubmit = (e) => {
       e.preventDefault();
-      if(!value) return;
+      if (!value) return;
       const status = false;
-       addTodo(value, status);
-       setValue('');
-    }
+      addTodo(value, status);
+      setValue('');
+    };
 
     return (
-      <form className="form" onSubmit={handleSubmit}>
-        <input id="add-todo" type="text" placeholder=" ADD TODO" onChange={e => setValue(e.target.value)}></input>
+      <form className='form' onSubmit={handleSubmit}>
+        <input
+          id='add-todo'
+          type='text'
+          placeholder=' ADD TODO'
+          onChange={(e) => setValue(e.target.value)}></input>
       </form>
-    )
+    );
   }
 
   const [todos, setTodos] = useState([
-    {"task" : "Todo App: Vanilla Javascript", "status" : true}, 
-    {"task" : "Todo App: React Javascript", "status" : true}, 
-    {"task" : "Weather App: Vanilla Javascript", "status" : false}, 
-    {"task" : "Weather App: React Javascript", "status" : false}, 
-    {"task" : "Complete Portfolio", "status" : false}
-  ])
+    { task: 'Escort a UFC fighter to the ring.', status: true },
+    { task: 'Walk the Camino de Santiago', status: true },
+    { task: 'Open for Alice Cooper and Iron Madien', status: true },
+    { task: 'Recieve 3 College Diplomas', status: true },
+    { task: 'Perform in 8 countries', status: true },
+    { task: 'Complete a marathon', status: true },
+  ]);
 
   const addTodo = (task, status) => {
-     const newTodos = [...todos, { task, status }];
-     setTodos(newTodos);
-  }
-
-  const completeTodo = index => {
-    const newTodos = [...todos];
-    newTodos[index].status = !newTodos[index].status
+    const newTodos = [...todos, { task, status }];
     setTodos(newTodos);
-  }
+  };
+
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].status = !newTodos[index].status;
+    setTodos(newTodos);
+  };
 
   const deleteTodos = () => {
     const newTodos = [];
     setTodos(newTodos);
-  }
-  
-  const deleteTodo = index => {
-    
+  };
+
+  const deleteTodo = (index) => {
     const newTodos = [...todos];
     const newerTodos = newTodos.filter((x) => {
-
-      return (!(newTodos[index].task === x.task)
-      )
-    })
+      return !(newTodos[index].task === x.task);
+    });
     setTodos(newerTodos);
-  }
+  };
 
-  
   return (
-    <div> 
+    <div>
       <header>
-        <div id="clear-tasks" onClick={() => deleteTodos()}>
-              <p>Clear All</p>
-          </div>
-          
-          <h1>TODO LIST</h1>
-          <TodoForm addTodo={addTodo}/>
+        <div id='clear-tasks' onClick={() => deleteTodos()}>
+          <p>Clear All</p>
+        </div>
+
+        <h1>TODO LIST</h1>
+        <TodoForm addTodo={addTodo} />
       </header>
       <main>
-                <div id="list">
-                    <ul id="todo-list">
-                        {todos.map((todo, index)=> (
-                            <Todos 
-                            key={index} 
-                            index={index} 
-                            todo={todo} 
-                            completeTodo={completeTodo}
-                            deleteTodo={deleteTodo}
-                            />
-                        ))}
-                    </ul>
-                </div>
-            </main>
-        <Footer />
+        <div id='list'>
+          <ul id='todo-list'>
+            {todos.map((todo, index) => (
+              <Todos
+                key={index}
+                index={index}
+                todo={todo}
+                completeTodo={completeTodo}
+                deleteTodo={deleteTodo}
+              />
+            ))}
+          </ul>
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 }
--- todo_react_js/src/Footer.js
+++ todo_react_js/src/Footer.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 export default function Footer() {
-    return (
-        <div>
-            <footer>
-                <p>Created with React JS</p>
-                <p>Todo List</p>
-                <p>© 2019 Christian Jurt</p>
-            </footer>
-        </div>
-    )
+  return (
+    <div>
+      <footer>
+        <p>Created with React JS</p>
+        <p>Todo List</p>
+        <p>© 2019 Christian Jurt</p>
+      </footer>
+    </div>
+  );
 }
